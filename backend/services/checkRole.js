585e7dd58786d1b265b59e9607a931c7ecde294f
Andrea <103192770+andreaabrami98@users.noreply.github.com>
2024-04-15 23:24:05 +0200

    Undo

--- backend/services/checkRole.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-function checkRole(req, res, next) {
-    if (res.locals && res.locals.role == process.env.USER)
-        res.sendStatus(401);
-    else next();
-}
-
-module.exports = { checkRole: checkRole }
